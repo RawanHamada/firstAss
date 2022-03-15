0c829fd870e164be98b5c7a13caffc061ab49699
RawanHamada <rawanahamada@gmail.com>
2022-03-15 09:00:01 +0200

    f

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,21 @@ class IugScreen extends StatelessWidget{
       appBar: AppBar(title: Text('عاصمة فلسطين'
       ,style:TextStyle(fontSize: 30 )),
       centerTitle: true,
-      backgroundColor: Color.fromARGB(255, 130, 40, 204),
+      backgroundColor: Color.fromARGB(255, 245, 237, 237),
 ),
     body: Column(children: [
       Image.asset('assets/img/quds.jpg'),
-      Center(child: Text('مدينة القدس'
-      ,style:TextStyle(fontSize: 30 ))),
+      Center(child: Container(
+        child: Text('مدينة القدس'
+        ,style:TextStyle(fontSize: 30,color: Color.fromARGB(108, 27, 25, 25) )),
+      )),SizedBox(height: 5),
+     
      Page1(label: ':الاسم ',value: 'القدس',),
      Page1(label: ':المساحة ',value: '١٢٥كم',),
      Page1(label: ':السكان ',value: '٣٥٨٠٠٠نسمة',),
      Page1(label: ':الدولة ',value: 'فلسطين',),
      Page1(label: ':اسم الطالب ',value: 'عمر أحمد علي',),
-    ],),
+    ],),backgroundColor: Color.fromARGB(0, 0, 0, 0),
     );
   }
 }
